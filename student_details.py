6271e3e06ee388ce9353ecf614bb9c8236bf474d
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-01 14:54:27 +0530

    Add files via upload

---
+++ student_details.py
@@ -0,0 +1,8 @@
+
+
+name=input("Enter the name of the student:")
+roll_number=int(input("Enter your roll_number"))
+cgp=float(input("Enter your cgp:"))
+print("name of the student:",name)
+print("roll_number:",roll_number)
+print("cgp of the student in percentage:",cgp*10,"%")
