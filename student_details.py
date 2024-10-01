ea5e66d1d2fec31610277b7ab68ebdba2c220503
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:46 +0530

    Add files via upload

--- student_details.py
+++ student_details.py
@@ -1,8 +1,10 @@
-
-
+from colorsys import yiq_to_rgb
+'''Author:job thomas cherian
+Date:01-10-2024
+Python programme to get the student details'''
 name=input("Enter the name of the student:")
-roll_number=int(input("Enter your roll_number"))
+Roll_number=int(input("Enter your Roll_number:"))
 cgp=float(input("Enter your cgp:"))
 print("name of the student:",name)
-print("roll_number:",roll_number)
-print("cgp of the student in percentage:",cgp*10,"%")
+print("Roll_number:",Roll_number)
+print("CGP of the student in percentage:",cgp*10,"%")
