cea2a372a012afbdabb6b6538151a129c8aecc9e
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-08 15:01:28 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,14 @@
+'''
+Author: Job Thomas Cherian
+Date:08/10/2024
+Simple desktop calculator using Python. Only the five basic arithmetic operators.
+'''
+first_name="job"
+last_name="thomas"
+full_name=first_name+' '+last_name
+print(full_name)
+
+length=len(first_name)
+print(length)
+extracted_first_name=full_name[length+1:]
+print(extracted_first_name)
