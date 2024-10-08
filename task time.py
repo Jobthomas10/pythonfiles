2e87793b5442986a7d11f91710090a04e60ed42b
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-08 16:14:47 +0530

    Add files via upload

---
+++ task time.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+current=datetime.now()
+print(current)
+currenttime=datetime.now()
+print(currenttime)
+format1=currenttime.strftime("%Y-%m-%d")
+print(format1)
+format2=currenttime.strftime("%d-%b-%Y")
+print(format2)
+format3=currenttime.strftime("%H:%M:%S:%p")
+print(format3)
