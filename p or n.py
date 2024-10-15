513296ee14c4d09e560fa94d9d32d18b2bf1be49
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-15 14:18:35 +0530

    Add files via upload

---
+++ p or n.py
@@ -0,0 +1,12 @@
+'''
+   Author : job thomas cherian
+   Date:15/10/2024
+   program to find whether a number is positive or negative
+   '''
+number=int(input("enter a number: "))
+if number > 0:
+    print ("the given number",number,"is positive")
+elif number<0:
+    print("the given number",number,"is negative")
+else:
+    print("the given number",number,"is zero")
