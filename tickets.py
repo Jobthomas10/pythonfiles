50200f16f0333c93a5322ba43ef89d47e4c6b9ae
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-10-15 15:03:09 +0530

    Add files via upload

---
+++ tickets.py
@@ -0,0 +1,12 @@
+'''
+Author:job thomas cherian
+Date:15/10/2024
+program to determine the smallest number
+'''
+age= int(input("Enter your age:"))
+if age <10:
+    print("$7")
+elif age>=10 and  age<60:
+    print("$10")
+else:
+    print("$5")
