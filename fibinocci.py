12d7f15cc9f44b1b8d0ff803a65d300036474b1f
Jobthomas10 <jobthomascherian2028@cs.sjcetpalai.ac.in>
2024-12-03 14:23:57 +0530

    Add files via upload

---
+++ fibinocci.py
@@ -0,0 +1,12 @@
+"""
+Author:Job thomas cherian
+Date:03/12/24
+"""
+def fibonacci(n):
+    sequence=[]
+    first_number,second_number=0,1
+    for i in range(n):
+        sequence.append(first_number)
+        first_number,second_number=second_number,first_number+second_number
+    return sequence
+print(fibonacci(10))
